cf9cfcfe5161520677b45cd5c7c6524174bacb30
Bartô Alheiros <alheirosb@gmail.com>
2021-03-25 20:55:55 -0300

    Variáveis/Início de Array

---
+++ main.js
@@ -0,0 +1,14 @@
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+console.log(lista);
+
+//var nome = "Bartô Alheiros";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Japão","Brasil"));
