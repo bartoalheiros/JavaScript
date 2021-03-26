6a7895963473db3923f614ab4a29d2b089bb1e04
Bartô Alheiros <alheirosb@gmail.com>
2021-03-26 08:54:04 -0300

    Condicionais, laços de repetição e Datas(Date)

--- main.js
+++ main.js
@@ -1,25 +1,86 @@
+/* var nome = "Bartô Alheiros";
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+alert(nome + " tem " + idade + " anos");
+alert(idade + idade2);
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão","Brasil")); */
+
 /* Dicionário: são estruturas caracterizadas
 por atributos de 'chave e valor'. Se assemelham ao formato .json */
 
-var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+/* var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
 console.log(frutas);
-alert(frutas[1].nome);
+alert(frutas[1].nome); */
 
 
-//var lista = ["maçã", "pêra", "laranja"];
-//lista.push("uva");
-//lista.pop();
-//lista.pop();
-//console.log(lista.toString());
-//console.log(lista.join(" - "));
+/* var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva");
+lista.pop();
+lista.pop();
+console.log(lista.toString());
+console.log(lista.join(" - ")); */
 
-//var nome = "Bartô Alheiros";
-//var n1 = 5;
-//var n2 = 3;
-//var frase = "Japão é o melhor time do mundo";
-//alert(nome + " tem " + idade + " anos");
-//alert(idade + idade2);
-//console.log(nome);
-//console.log(n1 * n2);
-//console.log(frase.toLowerCase());
-//alert(frase.replace("Japão","Brasil"));
+// Condicionais:
+
+/* var idade = prompt("Qual sua idade?");
+if (idade >= 18) {
+    alert("maior de idade");
+} else {
+    alert("menor de idade");
+} */
+
+/* Estruturas de repetição
+while: */
+
+/* var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}; */
+
+/* Estruturas de repetição
+for: */
+
+/* for(var count = 1; count <= 5; count++) {
+    console.log(count);
+    alert(count);
+} */
+
+// Datas:
+
+/* var d = new Date();
+alert("Mês: " + (d.getMonth()+1));
+alert("Minutos: " + d.getMinutes());
+alert("Dia da semana: " + d.getDay());
+alert("Ano: " + d.getFullYear()); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
