9c743a8994421942339fda8ce481b5c07bde302c
Bartô Alheiros <alheirosb@gmail.com>
2021-03-26 11:28:18 -0300

    Funções

--- main.js
+++ main.js
@@ -59,6 +59,27 @@ alert("Minutos: " + d.getMinutes());
 alert("Dia da semana: " + d.getDay());
 alert("Ano: " + d.getFullYear()); */
 
+// Funções:
+
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+var validar = 0;
+
+function validaIdade(idade) {
+    if (idade >= 18) {
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+validaIdade(idade);
+console.log(validar);
+
 
 
 
