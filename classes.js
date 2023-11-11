106aa627b42a2d423b452966fa2304f645bcbdd9
Bowie <marketing@letscode.com>
2023-11-11 09:41:52 -0300

    add: definições de classes e classe básica retangulo com método getter área

---
+++ classes.js
@@ -0,0 +1,7 @@
+// const p = new Retangulo(); // Erro de referência (ReferenceError)
+
+// class Retangulo {}
+
+class Retangulo {}
+
+const p = new Retangulo(); // Referência acessada da forma correta
